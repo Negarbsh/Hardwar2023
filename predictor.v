a18d68c583b8f947afa9a57f2f1761a561dc03da
Negar <babashahNegar@gmail.com>
2023-06-01 10:24:28 +0330

    Implement predictor

--- predictor.v
+++ predictor.v
@@ -1,5 +1,21 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-// Your code
+reg [1:0] counter;
+always @ (posedge clk)
+    begin
+        if (request)
+            prediction = counter[1];
+
+        else if (result)
+            begin
+                if (taken == 1'b1)
+                    if (counter != 2'b11)
+                        counter = counter + 1;
+                else 
+                    if (counter != 2'b00)
+                        counter = counter - 1;
+            end
+
+    end
 
 endmodule
