6254f1e4cd05949f4e7c1421000bf4fd04633ce5
Negar <babashahNegar@gmail.com>
2023-06-01 10:44:47 +0330

    Try
    Co-authored-by: hamraz arafati <hamriouz@users.noreply.github.com>
    Co-authored-by: Hasti Karimi <HastiKarimi@users.noreply.github.com>

--- predictor.v
+++ predictor.v
@@ -1,7 +1,10 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
 reg [1:0] counter = 2'b11;
-always @ (posedge clk)
+
+
+
+always@(posedge clk)
     begin
         if (request)
             prediction <= counter[1];
@@ -9,13 +12,14 @@ always @ (posedge clk)
         else if (result)
             begin
                 if (taken == 1'b1)
-                    if (counter != 2'b11)
-                        counter <= counter + 1;
+                    if (!(counter[0] == 1 && counter[1] == 1))
+                        counter = counter + 1;
                 else 
-                    if (counter != 2'b00)
-                        counter <= counter - 1;
+                    if (!(counter[0] == 0 && counter[1] == 0))
+                        counter = counter - 1;
             end
 
     end
 
 endmodule
+
